0ac39c1ee90d1ca5fc9bafcb569ffffa002b898a
titobran <159795287+titobran@users.noreply.github.com>
2025-09-03 11:28:15 -0500

    first commit

---
+++ Juan Diego python/Ejemplos 2.py
@@ -0,0 +1,6 @@
+#multiplicaciones
+print(2 ** 3)
+
+# Divisiones 
+print(15 / 4) # Division normal
+print(15 // 4) # DIvision 
---
+++ num1 = 60.py
@@ -0,0 +1,4 @@
+num1 = 60 
+num2 = 40.25
+print(float(num1))
+print(int(num2))
